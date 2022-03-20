dea3a77d117caabd0c5ead0e75f994d28d384ecc
Ricardo Costa <ricardocosta1989@gmail.com>
2022-03-20 14:08:55 +0000

    chore: Add linting and formatting
    Add ESLint and Prettier for linting and formatting, respectively.
    
    ESLint plugins:
    - React
    - React Hooks
    - Typescript
    - Testing Library
    - SonarJS
    - Compat
    
    Prettier plugins:
    - prettier-plugin-sort-imports
    
    Issue #1

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import logo from "./logo.svg";
+
 import "./App.css";
+import logo from "./logo.svg";
 
 function App() {
   const [count, setCount] = useState(0);
