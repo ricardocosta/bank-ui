28a6c634fb107a312e57392bd68f767b1041b7d5
Ricardo Costa <ricardocosta1989@gmail.com>
2022-10-29 23:24:25 +0100

    chore: Replace Trivago's prettier plugin with fork
    The Trivago prettier plugin to sort imports seems to be less active and
    with less features than this fork, so I'm replacing it.

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,13 @@
+// @ts-check
+
+/** @type {import("@ianvs/prettier-plugin-sort-imports").PrettierConfig} */
 module.exports = {
   printWidth: 100,
-  importOrder: ["<THIRD_PARTY_MODULES>", "^[./]"],
+  importOrder: ["<THIRD_PARTY_MODULES>", "^[./]", "<TYPES>", "<TYPES>^[./]"],
+  importOrderBuiltinModulesToTop: true,
+  importOrderMergeDuplicateImports: true,
   importOrderSeparation: true,
   importOrderSortSpecifiers: true,
   importOrderCaseInsensitive: true,
-  plugins: [require.resolve("@trivago/prettier-plugin-sort-imports")],
+  plugins: [require.resolve("@ianvs/prettier-plugin-sort-imports")],
 };
