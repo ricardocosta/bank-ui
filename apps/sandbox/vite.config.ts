3a5d460397297d6430df7b07e416c1464c003864
Ricardo Costa <ricardocosta1989@gmail.com>
2022-10-31 23:44:22 +0000

    chore: Add svgr plugin
    Add a plugin to load SVG files as React components.
    
    This commit also fixes the TSConfig that was causing React's JSX
    transform not to be recognized correctly in VSCode.

--- apps/sandbox/vite.config.ts
+++ apps/sandbox/vite.config.ts
@@ -1,21 +1,13 @@
-/// <reference types="vitest" />
 /// <reference types="vite/client" />
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), svgr(), tsconfigPaths()],
   server: {
     port: 3001,
-  },
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./setupTests.ts",
-    coverage: {
-      reporter: ["lcov", "text"],
-    },
   },
 });
