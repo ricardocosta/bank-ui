04c2332a1e9cc6beb4ca9b992f05df5ee6563106
Ricardo Costa <ricardocosta1989@gmail.com>
2023-01-08 18:01:26 +0000

    feat(shell): Add basic layout
    Add the basic layout of the application, with sidebar and application
    area.
    
    Issue #6

--- apps/shell/src/main.tsx
+++ apps/shell/src/main.tsx
@@ -1,16 +1,26 @@
+import { UIProvider } from "@ricardocosta/ui-provider";
+import { extendTheme } from "@ricardocosta/ui-theme";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 import App from "./App";
-
-import "./index.css";
 
 const container = document.getElementById("root");
 if (container) {
   const root = createRoot(container);
+  const theme = extendTheme();
   root.render(
     <StrictMode>
-      <App />
+      <HelmetProvider>
+        <UIProvider theme={theme}>
+          <Helmet>
+            <title>Bank</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          </Helmet>
+          <App />
+        </UIProvider>
+      </HelmetProvider>
     </StrictMode>
   );
 } else {
