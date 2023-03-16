45a158753063905a5d3ec06bffd9812eff020335
Ricardo Costa <ricardocosta1989@gmail.com>
2023-03-16 19:03:53 +0000

    chore: Allow URLs to go over line lengh limit
    Add a new local plugin to Commitlint that allows lines to be over the
    defined limit if they contain URLs, defined as containing http(s). This
    will allow adding links to release notes and other interesting web pages
    without breaking commitlint rules

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,8 +1,22 @@
+const MAX_BODY_LINE_LENGTH = 100;
+
+const lineLengthValidator = (arg) => {
+  const urlRegex = /https?:\/\//gi;
+  const linesAboveLimit = (arg ?? "")
+    .split("\n")
+    .filter((line) => line.length > MAX_BODY_LINE_LENGTH);
+
+  return [
+    linesAboveLimit.every((line) => line.match(urlRegex)) ? 2 : 0,
+    `Commit message contains lines above the ${MAX_BODY_LINE_LENGTH} characters limit.`,
+  ];
+};
+
 module.exports = {
   extends: ["@commitlint/config-conventional", "@commitlint/config-nx-scopes"],
   rules: {
     "subject-case": [2, "always", "sentence-case"],
-    "body-max-line-length": [2, "always", 80],
+    "body-max-line-length": [2, "always", 100],
     "type-enum": [
       2,
       "always",
@@ -21,4 +35,12 @@ module.exports = {
       ],
     ],
   },
+  plugins: [
+    {
+      rules: {
+        "body-max-line-length": ({ body }) => lineLengthValidator(body),
+        "footer-max-line-length": ({ footer }) => lineLengthValidator(footer),
+      },
+    },
+  ],
 };
