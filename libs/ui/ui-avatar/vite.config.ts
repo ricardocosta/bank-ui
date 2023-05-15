66370471d247670fd21369eb7ea28f8cb66fcf74
Ricardo Costa <ricardocosta1989@gmail.com>
2023-05-15 13:10:58 +0100

    build: Exclude dependencies from bundle in UI libs
    Exclude the dependencies from the bundle in UI libs as those will be
    included by the shell application.
    This commit also removes the building process for CommonJS, keeping
    support solely for ESModules.
    
    Issue #671

--- libs/ui/ui-avatar/vite.config.ts
+++ libs/ui/ui-avatar/vite.config.ts
@@ -1,29 +1,16 @@
 import { resolve } from "path";
 
+import excludeDependenciesFromBundle from "rollup-plugin-exclude-dependencies-from-bundle";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  plugins: [excludeDependenciesFromBundle()],
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
       name: "ui-avatar",
       fileName: "ui-avatar",
-    },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ["@emotion/react", "@emotion/styled", "framer-motion", "react", "react-dom"],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          "@emotion/react": "EmotionReact",
-          "@emotion/styled": "EmotionStyled",
-          "framer-motion": "FramerMotion",
-          react: "React",
-          "react-dom": "ReactDOM",
-        },
-      },
+      formats: ["es"],
     },
   },
 });
